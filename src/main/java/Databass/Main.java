291ae57ca1f61ac1959be46a17c1e799fc28c72d
Armyanec <aram.v.hovakyan@gmail.com>
2018-11-18 23:07:12 +0400

    A

---
+++ src/main/java/Databass/Main.java
@@ -0,0 +1,32 @@
+package Databass;
+
+
+
+//import Screaping.Company;
+//import Screaping.Officers;
+
+//import Screaping.Ufamanesime;
+//import Screaping.scap.Artical;
+//import Screaping.scap.Logicaaa;
+
+
+import Screap.Company;
+import Screap.Officers;
+import Screaping.scap.Logicaaa;
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+Officers officers = new Officers();
+officers.officerBackup();
+
+
+
+
+
+    }
+
+
+}
