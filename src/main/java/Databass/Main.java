f3d2d2878ebdb1d2ba671955061add98ec085f84
Armyanec <aram.v.hovakyan@gmail.com>
2018-11-20 14:14:18 +0400

    run

--- src/main/java/Databass/Main.java
+++ src/main/java/Databass/Main.java
@@ -1,21 +1,6 @@
 package Databass;
-
-
-
-//import Screaping.Company;
-//import Screaping.Officers;
-
-//import Screaping.Ufamanesime;
-//import Screaping.scap.Artical;
-//import Screaping.scap.Logicaaa;
-
-
-import Screap.Company;
 import Screap.Model;
-import Screap.Officers;
 import Screap.Overview;
-
-
 import java.io.IOException;
 
 
